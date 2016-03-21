c2bbbe017f0048961e06d0d508779df697521fc0
Ilvana <ibrankovic1@etf.unsa.ba>
2016-03-21 22:00:53 +0100

    Refactored Controllers for User and Movie

--- Kino-TimXY/src/main/java/kino/controller/MovieController.java
+++ Kino-TimXY/src/main/java/kino/controller/MovieController.java
@@ -1,8 +1,13 @@
 package kino.controller;
 
+import kino.configuration.ErrorGenerator;
+import kino.configuration.JsonMessageGenerator;
 import kino.model.ModelFactory;
 import kino.model.entities.Movie;
 import kino.model.presentation.MovieViewModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +22,7 @@ import java.util.Map;
 public class MovieController {
 
     private final ModelFactory modelFactory = ModelFactory.getInstance();
+    private static final Logger logger = LoggerFactory.getLogger(EventController.class);
 
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
     public ResponseEntity getMovies() {
@@ -27,10 +33,13 @@ public class MovieController {
                 MovieViewModel movieViewModel = new MovieViewModel(movie);
                 movieViewModels.add(movieViewModel);
             }
+            logger.info("Returning movies as JSON objects.");
             return new ResponseEntity(movieViewModels, HttpStatus.OK);
         } catch (NullPointerException e) {
+            logger.error("Could not create MovieViewModel. Movie is null", e);
             return new ResponseEntity(generateError("Movie not found."), HttpStatus.NOT_FOUND);
         } catch (Exception e) {
+            logger.error("Unknown exception.", e);
             return new ResponseEntity(generateError("Something unusual happened. Please try again later."), HttpStatus.NOT_FOUND);
         }
     }
@@ -38,36 +47,94 @@ public class MovieController {
     @RequestMapping(value = "/get/{id}", method = RequestMethod.GET, produces = "application/json")
     public ResponseEntity getMovie(@PathVariable("id") Integer id) {
         try {
-            Movie movie=modelFactory.MovieRepository().findOne(id);
-            MovieViewModel movieViewModel=new MovieViewModel(movie);
+            Movie movie = modelFactory.MovieRepository().findOne(id);
+            MovieViewModel movieViewModel = new MovieViewModel(movie);
+            logger.info(String.format("Returning movie with ID:%d as JSON object.", id));
             return new ResponseEntity(movieViewModel, HttpStatus.OK);
         } catch (IllegalArgumentException e) {
+            logger.error("Movie ID parameter type is unsupported", e);
             return new ResponseEntity(generateError("Movie ID parameter type is unsupported."), HttpStatus.NOT_ACCEPTABLE);
         } catch (NullPointerException e) {
+            logger.error(String.format("Movie not found for ID:%d.", id), e);
             return new ResponseEntity(generateError(String.format("Movie not found for ID:%d.", id)), HttpStatus.NOT_FOUND);
         } catch (Exception e) {
+            logger.error("Something unusual happened. Please try again later.", e);
             return new ResponseEntity(generateError("Something unusual happened. Please try again later."), HttpStatus.NOT_FOUND);
         }
     }
 
-    @RequestMapping(value = "/add",method=RequestMethod.POST, produces = "application/json")
-    public ResponseEntity addUser(@RequestBody Movie movie){
+    @RequestMapping(value = "/add", method = RequestMethod.POST, produces = "application/json")
+    public ResponseEntity add(@RequestBody Movie movie) {
         try {
             Movie savedMovie = modelFactory.MovieRepository().saveAndFlush(movie);
-            MovieViewModel movieViewModel=new MovieViewModel(savedMovie);
+            MovieViewModel movieViewModel = new MovieViewModel(savedMovie);
+            logger.info(String.format("Movie successfuly created. Movie ID: %d", movie.getId()));
             return new ResponseEntity(movieViewModel, HttpStatus.OK);
-        }
-        catch (Exception e){
-            return new ResponseEntity(generateError("Movie wasn't successfully added"),HttpStatus.BAD_REQUEST);
+        } catch (NullPointerException e) {
+            logger.error("Failed to create movie.", e);
+            return new ResponseEntity(
+                    ErrorGenerator.generateError("Failed to create movie."), HttpStatus.NOT_FOUND
+            );
+        } catch (Exception e) {
+            logger.error("Error occurred: ", e);
+            return new ResponseEntity(generateError("Movie wasn't successfully added"), HttpStatus.BAD_REQUEST);
         }
     }
 
-    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET, produces = "application/json")
-    public boolean deleteMovie(@PathVariable("id") Integer id) {
-        modelFactory.MovieRepository().delete(id);
-        Movie movie=modelFactory.MovieRepository().findOne(id);
-        if (movie == null) return true;
-        else return false;
+    @RequestMapping(value = "/update/{id}", method = RequestMethod.PUT)
+    public ResponseEntity update(@PathVariable("id") Integer id, @RequestBody Movie newMovie) {
+
+        try {
+            Movie movie = modelFactory.MovieRepository().findOne(id);
+            movie.setName(newMovie.getName());
+            movie.setDescription(newMovie.getDescription());
+            movie.setDuration(newMovie.getDuration());
+
+            modelFactory.MovieRepository().save(movie);
+            logger.info(String.format("Movie with ID: %d successfully updated.", movie.getId()));
+            return new ResponseEntity(new MovieViewModel(movie), HttpStatus.OK);
+        } catch (NullPointerException e) {
+            logger.error("Failed to update movie.", e);
+            return new ResponseEntity(
+                    ErrorGenerator.generateError("Failed to update movie."), HttpStatus.NOT_FOUND
+            );
+        } catch (Exception e) {
+            logger.error("Error occurred: ", e);
+            return new ResponseEntity(
+                    ErrorGenerator.generateError("Something went wrong."), HttpStatus.NOT_FOUND
+            );
+        }
+    }
+
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity delete(@PathVariable("id") Integer id) {
+        try {
+            modelFactory.MovieRepository().delete(id);
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(JsonMessageGenerator.generateMessage(
+                                    "Success", String.format("Movie wth ID: %d successfully deleted.", id))
+                    );
+        } catch (NullPointerException e) {
+            logger.error("Failed to delete movie.", e);
+            return new ResponseEntity(
+                    ErrorGenerator.generateError("Failed to delete movie."), HttpStatus.NOT_FOUND
+            );
+        } catch (IllegalArgumentException e) {
+            logger.error("The given id must not be null", e);
+            return new ResponseEntity(
+                    ErrorGenerator.generateError("The given id must not be null"), HttpStatus.NOT_ACCEPTABLE
+            );
+        } catch (EmptyResultDataAccessException e) {
+            logger.error(e.getMessage());
+            return new ResponseEntity(
+                    ErrorGenerator.generateError(e.getMessage()), HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            logger.error("Error occurred: ", e);
+            return new ResponseEntity(
+                    ErrorGenerator.generateError("Something went wrong."), HttpStatus.NOT_FOUND
+            );
+        }
     }
 
     private Map<String, String> generateError(String errorInfo) {
--- Kino-TimXY/src/main/java/kino/controller/UserController.java
+++ Kino-TimXY/src/main/java/kino/controller/UserController.java
@@ -1,9 +1,13 @@
 package kino.controller;
 
+import kino.configuration.ErrorGenerator;
+import kino.configuration.JsonMessageGenerator;
 import kino.model.ModelFactory;
-import kino.model.entities.Movie;
 import kino.model.entities.User;
 import kino.model.presentation.UserViewModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,19 +22,23 @@ import java.util.Map;
 public class UserController {
 
     private final ModelFactory modelFactory = ModelFactory.getInstance();
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
     public ResponseEntity getUsers() {
         try {
             List<User> users = modelFactory.UserRepository().findAll();
             List<UserViewModel> userViewModels = new ArrayList<>();
-            for(User user : users) {
+            for (User user : users) {
                 userViewModels.add(new UserViewModel(user));
             }
+            logger.info("Returning users as JSON objects.");
             return new ResponseEntity(userViewModels, HttpStatus.OK);
         } catch (NullPointerException e) {
+            logger.error("Could not create UserViewModel. User is null", e);
             return new ResponseEntity(generateError("User not found."), HttpStatus.NOT_FOUND);
         } catch (Exception e) {
+            logger.error("Unknown exception.", e);
             return new ResponseEntity(generateError("Something unusual happened. Please try again later."), HttpStatus.NOT_FOUND);
         }
     }
@@ -40,34 +48,92 @@ public class UserController {
         try {
             User user = modelFactory.UserRepository().findOne(id);
             UserViewModel userViewModel = new UserViewModel(user);
+            logger.info(String.format("Returning user with ID:%d as JSON object.", id));
             return new ResponseEntity(userViewModel, HttpStatus.OK);
         } catch (IllegalArgumentException e) {
+            logger.error("User ID parameter type is unsupported", e);
             return new ResponseEntity(generateError("User ID parameter type is unsupported."), HttpStatus.NOT_ACCEPTABLE);
         } catch (NullPointerException e) {
+            logger.error(String.format("User not found for ID:%d.", id), e);
             return new ResponseEntity(generateError(String.format("User not found for ID:%d.", id)), HttpStatus.NOT_FOUND);
         } catch (Exception e) {
+            logger.error("Something unusual happened. Please try again later.", e);
             return new ResponseEntity(generateError("Something unusual happened. Please try again later."), HttpStatus.NOT_FOUND);
         }
     }
 
-    @RequestMapping(value = "/add",method=RequestMethod.POST, produces = "application/json")
-    public ResponseEntity addUser(@RequestBody User user){
+    @RequestMapping(value = "/add", method = RequestMethod.POST, produces = "application/json")
+    public ResponseEntity add(@RequestBody User user) {
         try {
             User savedUser = modelFactory.UserRepository().saveAndFlush(user);
             UserViewModel userViewModel = new UserViewModel(savedUser);
+            logger.info(String.format("User successfuly created. User ID: %d", user.getId()));
             return new ResponseEntity(userViewModel, HttpStatus.OK);
-        }
-        catch (Exception e){
-            return new ResponseEntity(generateError("User wasn't successfully added"),HttpStatus.BAD_REQUEST);
+        } catch (NullPointerException e) {
+            logger.error("Failed to create user.", e);
+            return new ResponseEntity(
+                    ErrorGenerator.generateError("Failed to create user."), HttpStatus.NOT_FOUND
+            );
+        } catch (Exception e) {
+            logger.error("Error occurred: ", e);
+            return new ResponseEntity(generateError("User wasn't successfully added"), HttpStatus.BAD_REQUEST);
         }
     }
 
-    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET, produces = "application/json")
-    public boolean deleteUser(@PathVariable("id") Integer id) {
-        modelFactory.UserRepository().delete(id);
-        User user=modelFactory.UserRepository().findOne(id);
-        if (user == null) return true;
-        else return false;
+    @RequestMapping(value = "/update/{id}", method = RequestMethod.PUT)
+    public ResponseEntity update(@PathVariable("id") Integer id, @RequestBody User newUser) {
+        try {
+            User user = modelFactory.UserRepository().findOne(id);
+            user.setName(newUser.getName());
+            user.setAddress(newUser.getAddress());
+            user.setEmail(newUser.getEmail());
+            user.setNumber(newUser.getNumber());
+
+            modelFactory.UserRepository().save(user);
+            logger.info(String.format("User with ID: %d successfully updated.", user.getId()));
+            return new ResponseEntity(new UserViewModel(user), HttpStatus.OK);
+        } catch (NullPointerException e) {
+            logger.error("Failed to update user.", e);
+            return new ResponseEntity(
+                    ErrorGenerator.generateError("Failed to update user."), HttpStatus.NOT_FOUND
+            );
+        } catch (Exception e) {
+            logger.error("Error occurred: ", e);
+            return new ResponseEntity(
+                    ErrorGenerator.generateError("Something went wrong."), HttpStatus.NOT_FOUND
+            );
+        }
+    }
+
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE, produces = "application/json")
+    public ResponseEntity deleteUser(@PathVariable("id") Integer id) {
+        try {
+            modelFactory.UserRepository().delete(id);
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(JsonMessageGenerator.generateMessage(
+                                    "Success", String.format("User wth ID: %d successfully deleted.", id))
+                    );
+        } catch (NullPointerException e) {
+            logger.error("Failed to delete user.", e);
+            return new ResponseEntity(
+                    ErrorGenerator.generateError("Failed to delete user."), HttpStatus.NOT_FOUND
+            );
+        } catch (IllegalArgumentException e) {
+            logger.error("The given id must not be null", e);
+            return new ResponseEntity(
+                    ErrorGenerator.generateError("The given id must not be null"), HttpStatus.NOT_ACCEPTABLE
+            );
+        } catch (EmptyResultDataAccessException e) {
+            logger.error(e.getMessage());
+            return new ResponseEntity(
+                    ErrorGenerator.generateError(e.getMessage()), HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            logger.error("Error occurred: ", e);
+            return new ResponseEntity(
+                    ErrorGenerator.generateError("Something went wrong."), HttpStatus.NOT_FOUND
+            );
+        }
     }
 
     private Map<String, String> generateError(String errorInfo) {
