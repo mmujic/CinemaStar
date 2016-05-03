1fe455b902224483382c0cced2c293e7dea72d11
Zlatan Cilic <zlatan.cilic@atlantbh.com>
2016-05-03 17:45:43 +0200

    Initial work

---
+++ Kino-TimXY/src/main/java/kino/controller/SerializableMessageBundleController.java
@@ -0,0 +1,25 @@
+package kino.controller;
+
+import kino.utils.SerializableResourceBundleMessageSource;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Locale;
+import java.util.Properties;
+
+
+@Controller
+@RequestMapping("/messageBundle")
+public class SerializableMessageBundleController {
+
+    SerializableResourceBundleMessageSource messageBundle;
+
+    @RequestMapping(method = RequestMethod.GET)
+    @ResponseBody
+    public Properties list(@RequestParam String lang) {
+        return messageBundle.getAllProperties(new Locale(lang));
+    }
+}
