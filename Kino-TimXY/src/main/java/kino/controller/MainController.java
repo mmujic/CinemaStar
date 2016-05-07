f099562ce90e989e3d1f19f29001efe937025eae
Ilvana <ibrankovic1@etf.unsa.ba>
2016-05-08 00:15:29 +0200

    Added error handling

--- Kino-TimXY/src/main/java/kino/controller/MainController.java
+++ Kino-TimXY/src/main/java/kino/controller/MainController.java
@@ -3,6 +3,7 @@ package kino.controller;
 import kino.configuration.BeanConfiguration;
 import kino.model.entities.Contact;
 import kino.service.MailService;
+import kino.utils.ErrorGenerator;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.http.HttpStatus;
@@ -43,11 +44,17 @@ public class MainController {
     @RequestMapping(value = "/contactUs",method = RequestMethod.POST)
     @ResponseBody
     public ResponseEntity contactUs(@RequestBody Contact contact){
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(BeanConfiguration.class);
-        MailService mailService = applicationContext.getBean(MailService.class);
-        mailService.sendMail(contact.getEmail(),"mujic-m@hotmail.com",contact.getSubject(),contact.getMessage());
-        return new ResponseEntity(contact, HttpStatus.OK);
+        try {
+            ApplicationContext applicationContext = new AnnotationConfigApplicationContext(BeanConfiguration.class);
+            MailService mailService = applicationContext.getBean(MailService.class);
+            mailService.sendMail(contact.getEmail(), "mujic-m@hotmail.com", contact.getSubject(), contact.getMessage());
+            return new ResponseEntity(contact, HttpStatus.OK);
+        }
+        catch (Exception e) {
+            return new ResponseEntity(ErrorGenerator.generateError("Error happened during sending mail."), HttpStatus.BAD_REQUEST);
+        }
     }
+
     @RequestMapping(value = "/403", method = RequestMethod.GET)
     public String accessDenied() {
         return "403Page";
